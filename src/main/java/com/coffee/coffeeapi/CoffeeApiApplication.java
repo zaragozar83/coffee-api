62b637305c73ccb1a174f7ad92d2d6abd3736e5b
rocket-rzaragoza <rzaragoza@rocketlawyer.com>
2020-06-27 08:53:05 -0500

    First commit

---
+++ src/main/java/com/coffee/coffeeapi/CoffeeApiApplication.java
@@ -0,0 +1,13 @@
+package com.coffee.coffeeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coffee/coffeeapi/CoffeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coffee.coffeeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
