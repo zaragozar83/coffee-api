93986d931b9546db5652311e1ee748e46ff99ac0
rocket-rzaragoza <rzaragoza@rocketlawyer.com>
2020-06-27 23:03:06 -0500

    Add implementation endpoints for CRUD and use Mono and Flux

--- src/main/java/com/coffee/coffeeapi/CoffeeApiApplication.java
+++ src/main/java/com/coffee/coffeeapi/CoffeeApiApplication.java
@@ -1,7 +1,12 @@
 package com.coffee.coffeeapi;
 
+import com.coffee.coffeeapi.model.Product;
+import com.coffee.coffeeapi.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class CoffeeApiApplication {
@@ -9,5 +14,30 @@ public class CoffeeApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeeApiApplication.class, args);
 	}
+
+
+	/*@Bean
+	CommandLineRunner init(ProductRepository repository) {
+
+		return args -> {
+			Flux<Product> productFlux = Flux.just(
+					Product.builder()
+							.name("Big Latte")
+							.price(2.99)
+							.build(),
+					Product.builder()
+							.name("Big Decaf")
+							.price(2.49)
+							.build(),
+					Product.builder()
+							.name("Green Tea")
+							.price(1.99)
+							.build()
+					).flatMap(p -> repository.save(p));
+
+			productFlux.thenMany(repository.findAll())
+				.subscribe(System.out::println);
+		};
+	}*/
 
 }
---
+++ src/main/java/com/coffee/coffeeapi/model/Product.java
@@ -0,0 +1,24 @@
+package com.coffee.coffeeapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Builder
+@Document
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+  @Id
+  private String id;
+
+  private String name;
+
+  private Double price;
+
+}
