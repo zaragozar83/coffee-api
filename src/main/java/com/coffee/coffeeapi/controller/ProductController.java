979bbe48bcdf58bd5840954c9e1ef6ef5e06f37c
rocket-rzaragoza <rzaragoza@rocketlawyer.com>
2020-06-28 09:41:12 -0500

    Remove te Functional Endpoints

--- src/main/java/com/coffee/coffeeapi/controller/ProductController.java
+++ src/main/java/com/coffee/coffeeapi/controller/ProductController.java
@@ -21,66 +21,66 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-//@RestController
-//@RequestMapping("/products")
-//@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class ProductController {
 
-//  private final ProductRepository productRepository;
+  private final ProductRepository productRepository;
 
-//  @GetMapping
-//  public Flux<Product> getAllProducts() {
-//    return productRepository.findAll();
-//  }
+  @GetMapping
+  public Flux<Product> getAllProducts() {
+    return productRepository.findAll();
+  }
 
-//  @GetMapping("/{id}")
-//  public Mono<ResponseEntity<Product>> getProduct(@PathVariable String id) {
-//    return productRepository.findById(id)
-//        .map(product -> ResponseEntity.ok(product))
-//        .defaultIfEmpty(ResponseEntity.notFound().build());
-//  }
+  @GetMapping("/{id}")
+  public Mono<ResponseEntity<Product>> getProduct(@PathVariable String id) {
+    return productRepository.findById(id)
+        .map(product -> ResponseEntity.ok(product))
+        .defaultIfEmpty(ResponseEntity.notFound().build());
+  }
 
-//  @PostMapping
-//  @ResponseStatus(HttpStatus.CREATED)
-//  public Mono<Product> saveProduct(@RequestBody Product product) {
-//    return productRepository.save(product);
-//  }
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public Mono<Product> saveProduct(@RequestBody Product product) {
+    return productRepository.save(product);
+  }
 
-//  @PutMapping("/{id}")
-//  public Mono<ResponseEntity<Product>> updateProduct(@PathVariable String id,
-//                                                    @RequestBody Product product) {
-//
-//    return productRepository.findById(id)
-//            .flatMap(existingProduct -> {
-//              existingProduct.setName(product.getName());
-//              existingProduct.setPrice(product.getPrice());
-//              return productRepository.save(existingProduct);
-//            })
-//        .map(updateProduct -> ResponseEntity.ok(updateProduct))
-//        .defaultIfEmpty(ResponseEntity.notFound().build());
-//  }
+  @PutMapping("/{id}")
+  public Mono<ResponseEntity<Product>> updateProduct(@PathVariable String id,
+                                                    @RequestBody Product product) {
 
-//  @DeleteMapping("/{id}")
-//  public Mono<ResponseEntity<Void>> deleteProduct(@PathVariable String id) {
-//    return productRepository.findById(id)
-//                            .flatMap(existingProduct -> {
-//                              return productRepository.delete(existingProduct)
-//                                  .then(Mono.just(ResponseEntity.ok().<Void>build()));
-//                            })
-//                            .defaultIfEmpty(ResponseEntity.notFound().build());
-//  }
-//
-//  @DeleteMapping
-//  public Mono<Void> deleteAllProducts() {
-//    return productRepository.deleteAll();
-//  }
-//
-//  @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-//  public Flux<ProductEvent> getProductEvents() {
-//    return Flux.interval(Duration.ofSeconds(1))
-//            .map(v -> ProductEvent.builder()
-//                                  .eventId(v)
-//                                  .eventType("Product Event")
-//                                  .build());
-//  }
+    return productRepository.findById(id)
+            .flatMap(existingProduct -> {
+              existingProduct.setName(product.getName());
+              existingProduct.setPrice(product.getPrice());
+              return productRepository.save(existingProduct);
+            })
+        .map(updateProduct -> ResponseEntity.ok(updateProduct))
+        .defaultIfEmpty(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping("/{id}")
+  public Mono<ResponseEntity<Void>> deleteProduct(@PathVariable String id) {
+    return productRepository.findById(id)
+                            .flatMap(existingProduct ->
+                              productRepository.delete(existingProduct)
+                                  .then(Mono.just(ResponseEntity.ok().<Void>build()))
+                            )
+                            .defaultIfEmpty(ResponseEntity.notFound().build());
+  }
+
+  @DeleteMapping
+  public Mono<Void> deleteAllProducts() {
+    return productRepository.deleteAll();
+  }
+
+  @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+  public Flux<ProductEvent> getProductEvents() {
+    return Flux.interval(Duration.ofSeconds(1))
+            .map(v -> ProductEvent.builder()
+                                  .eventId(v)
+                                  .eventType("Product Event")
+                                  .build());
+  }
 }
